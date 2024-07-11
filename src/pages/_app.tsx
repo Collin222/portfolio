e0f74a205eba2da22fdc7b449513631a53ca799f
Collin <collin22h@gmail.com>
2024-07-11 17:26:43 -0500

    og

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,15 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     <div className={notoSans.className}>
       <Head>
         <title>collin22.dev | Web Dev and Design</title>
+
+        <meta
+          property="og:title"
+          content="Collin Cronin | Web Dev and Design"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://collin22.dev" />
+        <meta property="og:description" content="I build and design websites" />
+        <meta property="og:image" content="https://collin22.dev/ogimage.png" />
       </Head>
 
       <Component {...pageProps} />
